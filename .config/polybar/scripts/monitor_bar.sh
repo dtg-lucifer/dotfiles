481b98c9efa8f44aa5fffe220e009ffc077320c4
devpiush <dev.bosepiush@gmail.com>
2025-03-27 12:08:34 +0530

    i3: changed exec_always to only exec to run the monitor script

--- .config/polybar/scripts/monitor_bar.sh
+++ .config/polybar/scripts/monitor_bar.sh
@@ -1,7 +1,17 @@
+# #!/usr/bin/env bash
+#
+# # polybar-msg cmd quit
+#
+# xrandr --output HDMI-A-1-0 --rate 100 --mode 1920x1080 --right-of eDP
+# i3-msg restart > /dev/null
+# polybar monitor &
+# # polybar laptop &
+#
+# ------------------------------------------
+#
+
 #!/usr/bin/env bash
-
-# polybar-msg cmd quit
-
-xrandr --output HDMI-A-1-0 --rate 100 --mode 1920x1080 --right-of eDP
-i3-msg restart > /dev/null
-polybar monitor &
+export DISPLAY=:0
+xrandr --output HDMI-A-1-0 --rate 100 --mode 1920x1080 --right-of eDP 2>&1 | tee -a /tmp/monitor_script.log
+i3-msg restart > /dev/null 2>&1 | tee -a /tmp/monitor_script.log
+polybar monitor & disown
